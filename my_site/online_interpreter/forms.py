999419f388211ed3ec001c81429195e410ec8ac9
Roman Shentsev <3184447@mail.ru>
2021-01-21 14:34:58 +0300

    Added validation to forms.py + style for error messages + tests.

--- my_site/online_interpreter/forms.py
+++ my_site/online_interpreter/forms.py
@@ -1,8 +1,78 @@
+import re
+
 from django import forms
+from django.core.validators import RegexValidator
+
+
+regex_validator_open = RegexValidator(
+    regex=re.compile("open", flags=re.ASCII),
+    message="You can't use open function",
+    inverse_match=True,
+)
+regex_validator_eval = RegexValidator(
+    regex=re.compile("eval", flags=re.ASCII),
+    message="You can't use eval function",
+    inverse_match=True,
+)
+regex_validator_exec = RegexValidator(
+    regex=re.compile("exec", flags=re.ASCII),
+    message="You can't use exec function",
+    inverse_match=True,
+)
+regex_validator_os = RegexValidator(
+    regex=re.compile(r"[%0-9\b]?os[\b%0-9]?", flags=re.ASCII),
+    message="You can't use os module",
+    inverse_match=True,
+)
+regex_validator_subprocess = RegexValidator(
+    regex=re.compile("subprocess", flags=re.ASCII),
+    message="You can't use subprocess module",
+    inverse_match=True,
+)
+regex_validator_pathlib = RegexValidator(
+    regex=re.compile("pathlib", flags=re.ASCII),
+    message="You can't use pathlib module",
+    inverse_match=True,
+)
+regex_validator_fileinput = RegexValidator(
+    regex=re.compile("fileinput", flags=re.ASCII),
+    message="You can't use fileinput module",
+    inverse_match=True,
+)
+regex_validator_shutil = RegexValidator(
+    regex=re.compile("shutil", flags=re.ASCII),
+    message="You can't use shutil module",
+    inverse_match=True,
+)
+regex_validator_parent_path = RegexValidator(
+    regex=re.compile(r"\.\.[/\\]{1}", flags=re.ASCII),
+    message="You can't go to the parent path",
+    inverse_match=True,
+)
+regex_validator_ftp = RegexValidator(
+    regex=re.compile(r".?ftp.?", flags=re.ASCII),
+    message="You can't use ftp protocol",
+    inverse_match=True,
+)
 
 
 class CodeForm(forms.Form):
-    user_code = forms.CharField(widget=forms.Textarea, label=False)
+    user_code = forms.CharField(
+        widget=forms.Textarea,
+        label=False,
+        validators=[
+            regex_validator_open,
+            regex_validator_eval,
+            regex_validator_exec,
+            regex_validator_os,
+            regex_validator_subprocess,
+            regex_validator_pathlib,
+            regex_validator_fileinput,
+            regex_validator_shutil,
+            regex_validator_parent_path,
+            regex_validator_ftp,
+        ],
+    )
 
 
 class StdIOForm(forms.Form):
--- my_site/online_interpreter/tests.py
+++ my_site/online_interpreter/tests.py
@@ -1,3 +1,6 @@
+import tempfile
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import LiveServerTestCase, SimpleTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
 
@@ -22,7 +25,7 @@ print(f(input()))
 """
 
 
-class FunctionalTests(LiveServerTestCase):
+class FunctionalTests(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -40,17 +43,17 @@ class FunctionalTests(LiveServerTestCase):
         user_code.send_keys("print('Hello world')")
         self.browser.find_element_by_id("id_launch").click()
         std_io = self.browser.find_element_by_id("id_std_io")
-        self.assertMultiLineEqual(std_io.text, ">>>\nHello world")
+        self.assertMultiLineEqual(std_io.text, "Hello world")
 
     def test_user_input(self):
         self.browser.get("%s%s" % (self.live_server_url, ""))
         user_code = self.browser.find_element_by_id("id_user_code")
         user_code.send_keys(CODE)
         std_io = self.browser.find_element_by_id("id_std_io")
-        std_io.send_keys("Test\nTest2")
+        std_io.send_keys("Line_1\nLine_2")
         self.browser.find_element_by_id("id_launch").click()
         std_io = self.browser.find_element_by_id("id_std_io")
-        self.assertMultiLineEqual(std_io.text, ">>> Test\nTest2\n>>> Test\nTest2")
+        self.assertMultiLineEqual(std_io.text, "Line_1\nLine_2\nLine_1\nLine_2")
 
     def test_erroneous_code(self):
         self.browser.get("%s%s" % (self.live_server_url, ""))
@@ -59,3 +62,92 @@ class FunctionalTests(LiveServerTestCase):
         self.browser.find_element_by_id("id_launch").click()
         std_io = self.browser.find_element_by_id("id_std_io")
         self.assertIn("SyntaxError", std_io.text)
+
+    def test_validator_open(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("open('script.py')")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use open function", errorlist.text)
+
+    def test_validator_eval(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("eval('1 + 1')")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use eval function", errorlist.text)
+
+    def test_validator_exec(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("exec('print(999)')")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use exec function", errorlist.text)
+
+    def test_validator_os(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("__import__('os').popen('COMMAND').read()")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use os module", errorlist.text)
+
+    def test_validator_subprocess(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("eval(compile(\"__import__('subprocess')\",'','single'))")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use subprocess module", errorlist.text)
+
+    def test_validator_pathlib(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("eval(compile(\"__import__('pathlib')\",'','single'))")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use pathlib module", errorlist.text)
+
+    def test_validator_fileinput(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("eval(compile(\"__import__('fileinput')\",'','single'))")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use fileinput module", errorlist.text)
+
+    def test_validator_shutil(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("eval(compile(\"__import__('shutil')\",'','single'))")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use shutil module", errorlist.text)
+
+    def test_validator_parent_path(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("cd ../../")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't go to the parent path", errorlist.text)
+
+    def test_validator_ftp(self):
+        self.browser.get("%s%s" % (self.live_server_url, ""))
+        user_code = self.browser.find_element_by_id("id_user_code")
+        user_code.send_keys("sftp://example.com")
+        self.browser.find_element_by_id("id_launch").click()
+        errorlist = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("You can't use ftp protocol", errorlist.text)
+
+
+# >>> with tempfile.NamedTemporaryFile(mode='w+') as fp:
+# ...     fp.write('Hello world!')
+# ...     fp.name
+# ...     fp.seek(0)
+# ...     fp.read()
+# ...
+# FileNotFoundError: [Errno 2] No such file or directory: '/tmp/tmpf33e7l33'
